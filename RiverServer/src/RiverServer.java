995643f1be9549e1dd7814123df19c00e86924af
Stefan Ilic <silic.hba2023@ivey.ca>
2021-08-26 12:04:15 -0400

    Upgraded so that the server now works for multiple clients

--- RiverServer/src/RiverServer.java
+++ RiverServer/src/RiverServer.java
@@ -1,4 +1,5 @@
 import java.net.*;
+import java.util.ArrayList;
 import java.io.*;
 
 public class RiverServer {
@@ -6,14 +7,16 @@ public class RiverServer {
 	private final int PORT_NUM = 3333;
 	private ServerSocket ss;  
 	private Socket s;  // will become local variable
-	private DataInputStream din;  // will
-	private DataOutputStream dout;  // will
+	ArrayList<ServerThread> allClients = new ArrayList<ServerThread>();
+
+	//private DataInputStream din;  // will
+	//private DataOutputStream dout;  // will
 	
 	private RiverServer () throws IOException{
 		ss=new ServerSocket(PORT_NUM);  
-		s=ss.accept();  // will be moved down to the run server method
-		din=new DataInputStream(s.getInputStream());  // move
-		dout=new DataOutputStream(s.getOutputStream());  // move
+		//s=ss.accept();  // will be moved down to the run server method ///////////////////
+		//din=new DataInputStream(s.getInputStream());  // move
+		//dout=new DataOutputStream(s.getOutputStream());  // move
 	}
 	
 	public static RiverServer getInstance() {
@@ -30,25 +33,46 @@ public class RiverServer {
 	}
 	
 	public void runServer() throws IOException{
+		
+		//Socket s;
 		try {
 			String str="";
-			while(!str.equals("stop")){ 
-				System.out.println(str);
-				str=din.readUTF();    
-				dout.writeUTF(str);
-				dout.flush();  
+			while(!str.equals("QUIT")){ 
+				
+				s=ss.accept(); ////////////////////////
+				ServerThread servT = new ServerThread(s, this);
+				servT.start();/////////////
+				allClients.add(servT);
+				
+				
+				
+				//System.out.println(str);
+				//str=din.readUTF();    
+				//dout.writeUTF(str);
+				//dout.flush();  
 			}
 		}catch(IOException io) {
 			System.out.println("Failed in run");
 		}
 	}
 	
+	public void sendMessage(String line) {
+		for(ServerThread st : allClients) {
+			if(st.isAlive()) {
+				st.write(line);
+			}else {
+				allClients.remove(st);
+			}
+		}
+		
+	}
+	
 	public void closeServer() throws IOException{
 		try {
 			System.out.println("Server Closed");
-			din.close(); 
-			dout.close();////
-			s.close();  
+			//din.close(); 
+			//dout.close();////
+			//s.close();  
 			ss.close();
 		}catch(IOException io) {
 			System.out.println("Failed to close");
